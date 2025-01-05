5c52eb4dd7a9dd44f8edb9729e9f524ef05439c4
sizukutamago <sizukutamago@gmail.com>
2025-01-05 14:25:44 +0900

    add url test

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -1,4 +1,5 @@
 use alloc::string::{String, ToString};
+use alloc::vec::Vec;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Url {
@@ -28,7 +29,7 @@ impl Url {
         self.host = self.extract_host();
         self.port = self.extract_port();
         self.path = self.extract_path();
-        self.searchpart = self.extract_seacrhpart();
+        self.searchpart = self.extract_searchpart();
 
         Ok(self.clone())
     }
@@ -62,7 +63,7 @@ impl Url {
             .collect();
 
         if let Some(index) = url_parts[0].find(':') {
-            url_parts[0][..index + 1..].to_string()
+            url_parts[0][index + 1..].to_string()
         } else {
             "80".to_string()
         }
@@ -75,7 +76,6 @@ impl Url {
             .splitn(2, "/")
             .collect();
 
-
         if url_parts.len() < 2 {
             return "".to_string();
         }
@@ -84,23 +84,23 @@ impl Url {
         path_and_searchpart[0].to_string()
     }
 
-    fn extract_seacrhpart(&self) -> String {
+    fn extract_searchpart(&self) -> String {
         let url_parts: Vec<&str> = self
             .url
             .trim_start_matches("http://")
             .splitn(2, "/")
             .collect();
 
-            if url_parts.len() < 2 {
-                return "".to_string();
-            }
-    
-            let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
-            if path_and_searchpart.len() < 2 {
-                "".to_string()
-            } else {
-                path_and_searchpart[1].to_string()
-            }
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+        if path_and_searchpart.len() < 2 {
+            "".to_string()
+        } else {
+            path_and_searchpart[1].to_string()
+        }
     }
 
     pub fn host(&self) -> String {
@@ -117,5 +117,85 @@ impl Url {
 
     pub fn searchpart(&self) -> String {
         self.searchpart.clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_url_host() {
+        let url = "http://example.com".to_string();
+
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "80".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_url_host_port() {
+        let url = "http://example.com:8888".to_string();
+
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "8888".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_url_host_port_path() {
+        let url = "http://example.com:8888/index.html".to_string();
+
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "8888".to_string(),
+            path: "index.html".to_string(),
+            searchpart: "".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_url_host_path() {
+        let url = "http://example.com/index.html".to_string();
+
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "80".to_string(),
+            path: "index.html".to_string(),
+            searchpart: "".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_url_host_path_searchpart() {
+        let url = "http://example.com:8888/index.html?a=123&b= 456".to_string();
+
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "8888".to_string(),
+            path: "index.html".to_string(),
+            searchpart: "a=123&b= 456".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
     }
 }
