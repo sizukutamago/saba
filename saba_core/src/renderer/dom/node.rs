c18b54bf4fe96cd77ed5e208d1c18ca6692a8c50
sizukutamago <sizukutamago@gmail.com>
2025-01-24 23:53:41 +0900

    dom

---
+++ saba_core/src/renderer/dom/node.rs
@@ -0,0 +1,77 @@
+use core::cell::RefCell;
+
+use alloc::rc::{Rc, Weak};
+
+#[derive(Debug, Clone)]
+pub struct Node {
+    pub kind: NodeKind,
+    window: Weak<RefCell<Window>>,
+    parent: Weak<RefCell<Node>>,
+    first_child: Option<Rc<RefCell<Node>>>,
+    last_child: Weak<RefCell<Node>>,
+    previous_sibling: Weak<RefCell<Node>>,
+    next_sibling: Option<Weak<RefCell<Node>>>,
+}
+
+impl Node {
+    pub fn new(kind: NodeKind) -> Self {
+        Self {
+            kind,
+            window: Weak::new(),
+            parent: Weak::new(),
+            first_child: None,
+            last_child: Weak::new(),
+            previous_sibling: Weak::new(),
+            next_sibling: None,
+        }
+    }
+
+    pub fn set_parent(&mut self, parent: Weak<RefCell<Node>>) {
+        self.parent = parent;
+    }
+
+    pub fn parent(&self) -> Weak<RefCell<Node>> {
+        self.parent.clone()
+    }
+    pub fn set_first_child(&mut self, first_child: Option<Rc<RefCell<Node>>>) {
+        self.first_child = first_child;
+    }
+
+    pub fn first_child(&self) -> Option<Rc<RefCell<Node>>> {
+        self.first_child.clone()
+    }
+
+    pub fn set_last_child(&mut self, last_child: Weak<RefCell<Node>>) {
+        self.last_child = last_child;
+    }
+
+    pub fn last_child(&self) -> Weak<RefCell<Node>> {
+        self.last_child.clone()
+    }
+
+    pub fn set_previous_sibling(&mut self, previous_sibling: Weak<RefCell<Node>>) {
+        self.previous_sibling = previous_sibling;
+    }
+
+    pub fn previous_sibling(&self) -> Weak<RefCell<Node>> {
+        self.previous_sibling.clone()
+    }
+
+    pub fn set_next_sibling(&mut self, next_sibling: Option<Weak<RefCell<Node>>>) {
+        self.next_sibling = next_sibling;
+    }
+
+    pub fn next_sibling(&self) -> Option<Weak<RefCell<Node>>> {
+        self.next_sibling.as_ref().cloned()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum NodeKind {
+    /// https://dom.spec.whatwg.org/#interface-document
+    Document,
+    /// https://dom.spec.whatwg.org/#interface-element
+    Element,
+    /// https://dom.spec.whatwg.org/#interface-text
+    Text,
+}
