64f496b95aea29377b056dcbe45cac36a2b6d1d6
sizukutamago <sizukutamago@gmail.com>
2025-02-07 23:13:42 +0900

    css token

---
+++ saba_core/src/renderer/css/token.rs
@@ -0,0 +1,44 @@
+use alloc::{string::String, vec::Vec};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum CssToken {
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-hash-token
+    HashToken(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-delim-token
+    Delim(char),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-number-token
+    Number(f64),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-colon-token
+    Colon,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-semicolon-token
+    SemiColon,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-open-paren
+    OpenParenthesis,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-close-paren
+    CloseParenthesis,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-open-curly
+    OpenCurly,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-close-curly
+    CloseCurly,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-ident-token
+    Ident(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-string-token
+    StringToken(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-at-keyword-token
+    AtKeyword(String),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CssTokenizer {
+    pos: usize,
+    input: Vec<char>,
+}
+
+impl CssTokenizer {
+    pub fn new(input: String) -> Self {
+        Self {
+            pos: 0,
+            input: input.chars().collect(),
+        }
+    }
+}
